b472f7f628d879612346015d1f6a78ccdff6fbe8
alx <aleksandar.zedi@gmail.com>
2023-10-12 18:54:07 +0200

    Update Profile page to use getStaticProps to cache and prerender/prefetch the user data

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,17 +1,29 @@
 // libs
+import type { GetStaticProps, NextPage } from 'next';
 import Head from "next/head";
+import { createServerSideHelpers } from '@trpc/react-query/server';
+import superjson from 'superjson';
 
 // components/modules
 import CenterComponent from '~/modules/layout/center.component';
 
 // utils
+import { db } from '~/server/db';
 import { api } from '~/utils/api';
+import { appRouter } from '~/server/api/root';
 
-export default function ProfilePage() {
-  const { data, isError, isLoading } = api.profile.getUserByUserName.useQuery({ username: 'zedy' });
+/**
+ * @comment IT WORKS! after 3h debugging! 
+ * 
+ * solution: restart the server 🤦‍♂️
+ * 
+ */
+const ProfilePage: NextPage<{username: string}> = ({ username }) => {
+  const { data, isError, isLoading } = api.profile.getUserByUserName.useQuery({ username });
 
   if (isLoading) {
     // todo
+    console.log('this will never execute 🤞');
     return <div>Loading ...</div>
   }
 
@@ -24,7 +36,7 @@ export default function ProfilePage() {
   return (
     <>
       <Head>
-        <title>Profile</title>
+        <title>{data?.userName} profile page</title>
       </Head>
       <CenterComponent>
         <p>welcome to {`${data?.userName}'s`} profile page</p>
@@ -32,3 +44,53 @@ export default function ProfilePage() {
     </>
   );
 };
+
+/**
+ * @see https://trpc.io/docs/client/nextjs/server-side-helpers
+ * 
+ * The point of this is to prefetch the data of the profile match
+ * so that we have it on page load and inject data into the Meta.
+ * 
+ * @requires https://nextjs.org/docs/messages/invalid-getstaticpaths-value
+ * 
+ * @param context 
+ * @returns 
+ */
+export const getStaticProps: GetStaticProps = async (context) => {
+  const helpers = createServerSideHelpers({
+    router: appRouter,
+    ctx: {
+      db, userId: null, session: null,
+    },
+    transformer: superjson,
+  });
+
+  const slug = context.params?.slug;
+
+  if (!slug || typeof slug !== 'string') {
+    throw new Error('slug error'); // todo: think of something better
+  }
+
+  const parsedSlug = slug.replace('@', '');
+
+  await helpers.profile.getUserByUserName.prefetch({ username: parsedSlug });
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+      username: parsedSlug,
+    },
+  };
+};
+
+/**
+ * @see https://nextjs.org/docs/messages/invalid-getstaticpaths-value
+ */
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+};
+
+export default ProfilePage;
